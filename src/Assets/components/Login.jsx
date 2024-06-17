f43076160e4c5f5e5ab5f3371d944005bbf70b97
juan.arias <juan.ariasr@sansano.usm.cl>
2024-06-16 22:18:25 -0400

    update liogin

--- src/Assets/components/Login.jsx
+++ src/Assets/components/Login.jsx
@@ -1,9 +1,23 @@
-import react from 'react'
+import react, {useState} from 'react'
+import appFirebase from '../../credentials'
+import {getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword} from 'firebase/auth'
+import firebase from 'firebase/compat/app'
+
+const auth = getAuth(appFirebase)
 
 const Login = () => {
+
+    const [registrando, setRegistrando] = useState(false)
+
     return (
         <div className='container'>
-            <h1>wenactm</h1>
+            <h1>PRUEBA</h1>
+            <form>
+                <input type="text" placeholder='email'/>
+                <input type="text" placeholder='contrasena'/>
+                <button>{registrando ? "Registrate" : "Inicia Sesion"}</button>
+            </form>
+            <h4>{registrando ? "Si ya tienes cuenta" : "No tienes cuenta"}<button onClick={() => setRegistrando(!registrando)}>{registrando ? "Inicia Sesion": "Registrate"}</button></h4>
         </div>
     )
 }
