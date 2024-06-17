c7739181e94378289d653c052632d1ce21359949
juan.arias <juan.ariasr@sansano.usm.cl>
2024-06-16 20:17:15 -0400

    asd

--- src/credentials.js
+++ src/credentials.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAIxxVbhOzi7VY8fbhRFlMNvOuEgzHRTFM",
-  authDomain: "iatu-2b7a5.firebaseapp.com",
-  projectId: "iatu-2b7a5",
-  storageBucket: "iatu-2b7a5.appspot.com",
-  messagingSenderId: "86398412636",
-  appId: "1:86398412636:web:cc12ff1808600f04098399",
-  measurementId: "G-Y2S9WWDXB0"
+  apiKey: "AIzaSyAlAjh3ZFeW-BUdLM4L6_WslJGPywV4lyY",
+  authDomain: "iatu-pmv.firebaseapp.com",
+  projectId: "iatu-pmv",
+  storageBucket: "iatu-pmv.appspot.com",
+  messagingSenderId: "149128003789",
+  appId: "1:149128003789:web:193f5ed82edebcf7116984",
+  measurementId: "G-S2WLPZST4E"
 };
 
 // Initialize Firebase
